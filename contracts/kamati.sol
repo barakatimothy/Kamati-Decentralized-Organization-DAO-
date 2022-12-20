b4726a2d8340bc4131dbc74e971b28518e2e08be
barakatimothy <timothybaraka39@gmail.com>
2022-12-20 14:46:14 +0300

    version change

---
+++ contracts/kamati.sol
@@ -0,0 +1,80 @@
+pragma solidity ^0.8.0;
+
+// This is a simple DAO contract that allows members to propose and vote on actions.
+
+// The contract has a membership list, and any member can propose an action by
+// calling the propose() function and specifying the action details.
+
+// Other members can then vote on the proposal using the vote() function. If
+// the proposal receives more than 50% of the votes, it is considered passed
+// and the action is executed.
+
+// The contract also has a fallback function that allows members to pay fees or
+// make donations to the DAO.
+
+// Replace "YOUR_CONTRACT_NAME_HERE" and "YOUR_TOKEN_NAME_HERE" with your own names.
+
+// Import the SafeMath library to provide safe arithmetic operations.
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+// Import the ERC20 interface to use the standard functions for an ERC20 token.
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+
+// Import the IERC20 interface to use the standard functions for an IERC20 token.
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+// Create the contract.
+contract Kamati is SafeERC20 {
+    using SafeMath for uint256;
+
+    // Define the membership structure.
+    struct Member {
+        bool isActive;
+        uint256 votes;
+        string name;
+    }
+
+    // Define the proposal structure.
+    struct Proposal {
+        string description;
+        uint256 votesFor;
+        uint256 votesAgainst;
+        bool executed;
+        address recipient;
+        uint256 value;
+        bytes data;
+    }
+
+    // Define the events that can be emitted by the contract.
+    event NewProposal(uint256 proposalID);
+    event Vote(uint256 proposalID, bool vote);
+    event Execution(uint256 proposalID);
+
+    // Create the mapping for membership data.
+    mapping(address => Member) public members;
+
+    // Create the array for proposal data.
+    Proposal[] public proposals;
+
+    // Set the contract owner.
+    address public owner;
+
+    // Set the contract balance.
+    uint256 public balance;
+
+    // Set the contract name and symbol for ERC20 compliance.
+    string public name = "YOUR_TOKEN_NAME_HERE";
+    string public symbol = "YOUR_TOKEN_SYMBOL_HERE";
+    uint8 public decimals = 18;
+
+    // Set the contract constructor.
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    // Create the function to add a new member to the DAO.
+    function addMember(string memory _name) public {
+        require(msg.sender == owner, "Only the owner can add new members.");
+        require(_name.length > 0, "A name must be specified.");
+        members[msg.sender].isActive = true;
+        members[msg.sender
